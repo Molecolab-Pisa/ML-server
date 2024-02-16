847c4ac93f2db3ec467412849146845c70e8ae7d
Edoardo Cignoni <edoardo.cignoni96@gmail.com>
2024-02-16 11:33:04 +0100

    corrected setup and added example on server and client

---
+++ sander_orca/cli.py
@@ -0,0 +1,6 @@
+def server():
+    print('Hi, I\'m the server')
+
+
+def orca_client():
+    print('Hi, I\'m the client')
--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 from setuptools import find_packages, setup
 
+entry_points = {
+    'console_scripts': [
+        'ml_server=sander_orca.cli:server',
+        'orca=sander_orca.cli:orca_client',
+    ]
+}
+
 setup(
     name="sander_orca",
     version="0.0.0",
-    url="https://molimen1.dcci.unipi.it/p.mazzeo/sander-orca.git",
-    author="Patrizia Mazzeo, Edoardo Cignoni"
-    author_email="mazzeo.patrizia.1998@gmail.com, edoardo.cignoni96@gmail.com"
+    url="https://molimen1.dcci.unipi.it/p.mazzeo/sander_orca.git",
+    author="Patrizia Mazzeo, Edoardo Cignoni",
+    author_email="mazzeo.patrizia.1998@gmail.com, edoardo.cignoni96@gmail.com",
     description=open("README.md").read(),
+    entry_points=entry_points,
     packages=find_packages(),
     install_requires=["numpy"],
 )
