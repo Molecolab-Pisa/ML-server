b8c778c861f2ed28ca2114ab62b632afc977c4f5
Edoardo Cignoni <edoardo.cignoni96@gmail.com>
2024-02-16 12:16:53 +0100

    added function to read the inpfile.xyz and fixed a bug in read_ptchrg (only x coordinate was returned)

--- sander_orca/io.py
+++ sander_orca/io.py
@@ -11,9 +11,9 @@ import numpy as np
 
 
 def read_ptchrg(path: str) -> Tuple[int, np.ndarray, np.ndarray]:
-    """reads the ptchrg.xyz file provided by SANDER
+    """reads the ptchrg.xyz file provided by sander
 
-    The ptchrg file contains the number of atoms, the
+    The ptchrg file contains the number of mm atoms, the
     coordinates of the atoms, and the charges.
 
     Args:
@@ -38,6 +38,34 @@ def read_ptchrg(path: str) -> Tuple[int, np.ndarray, np.ndarray]:
         )
     # charges are in the first column
     q = xyz[:, 0]
-    xyz = xyz[:, 1]
+    xyz = xyz[:, 1:]
 
     return num_atoms, xyz, q
+
+
+def read_inpfile(path: str) -> Tuple[int, np.ndarray, np.ndarray]:
+    """reads the inpfile.xyz file provided by sander
+
+    The inpfile contains the number of qm atoms, the element of each atom
+    (e.g., H, C, O, ...), and the coordinates.
+
+    Args:
+        path: path to inpfile.xyz file
+
+    Returns:
+        num_atoms: number of atoms
+        xyz: atom coordinates, shape (num_atoms, 3)
+        elems: atom elements, shape (num_atoms)
+    """
+    # read the number of atoms
+    with open(path, 'r') as handle:
+        line = handle.readline()
+        num_atoms = int(line.strip())
+
+    # read the coordinates and the atom types
+    # skip two lines (the num_atoms and an empty line)
+    xyz = np.loadtxt(path, skiprows=2, dtype=str)
+    elems = xyz[:, 0]
+    xyz = xyz[:, 1:].astype(np.float64)
+
+    return num_atoms, xyz, elems
