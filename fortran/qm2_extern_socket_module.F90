10961420422c7fdb1839c39a6288055cc5ba6a13
p.mazzeo <mazzeo.patrizia.1998@gmail.com>
2024-07-25 10:38:46 +0200

    Added License

--- fortran/qm2_extern_socket_module.F90
+++ fortran/qm2_extern_socket_module.F90
@@ -1,3 +1,21 @@
+!  ML-server, Python script that allows sending energies and gradients 
+!  to Sander (Amber) to perform QM/MM simulations.
+!
+!  Copyright (C) 2024, Patrizia Mazzeo and Edoardo Cignoni and 
+!  Lorenzo Cupellini and Benedetta Mennucci
+!
+!  This program is free software: you can redistribute it and/or modify
+!  it under the terms of the GNU General Public License as published by
+!  the Free Software Foundation, either version 3 of the License, or
+!  (at your option) any later version.
+!
+!  This program is distributed in the hope that it will be useful,
+!  but WITHOUT ANY WARRANTY; without even the implied warranty of
+!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!  GNU General Public License for more details.
+!
+!  You should have received a copy of the GNU General Public License
+!  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include "../include/dprec.fh"
 module qm2_extern_socket_module
 ! ----------------------------------------------------------------
@@ -7,7 +25,8 @@ module qm2_extern_socket_module
 ! pure ML
 ! ML/MM with cutoff for ML-MM electrostatics 
 !
-! Authors: Patrizia Mazzeo and Edoardo Cignoni
+! Authors: Patrizia Mazzeo and Edoardo Cignoni 
+!          and Lorenzo Cupellini and Benedetta Mennucci
 ! Based on qm2_extern_orc_module.f
 !
 ! Date: June 2024
--- sander_orca/models/basemodels.py
+++ sander_orca/models/basemodels.py
@@ -1,3 +1,21 @@
+#  ML-server, Python script that allows sending energies and gradients 
+#  to Sander (Amber) to perform QM/MM simulations.
+#
+#  Copyright (C) 2024, Patrizia Mazzeo and Edoardo Cignoni and 
+#  Lorenzo Cupellini and Benedetta Mennucci
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 from __future__ import annotations
 
 import os
