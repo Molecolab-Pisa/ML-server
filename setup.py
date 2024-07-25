fb7a7bfb9bf341dafc3827f7f8b86658b2c42ca2
p.mazzeo <mazzeo.patrizia.1998@gmail.com>
2024-07-25 10:44:05 +0200

    Changed name

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from setuptools import find_packages, setup
 
 entry_points = {
     "console_scripts": [
-        "ml-server=sander_orca.cli:server",
-        "orca=sander_orca.cli:orca_client",
-        "ml-stop=sander_orca.cli:stop_server",
+        "ml-server=ml_server.cli:server",
+        "orca=ml_server.cli:orca_client",
+        "ml-stop=ml_server.cli:stop_server",
     ]
 }
 
 setup(
-    name="sander_orca",
-    version="0.0.0",
-    url="https://molimen1.dcci.unipi.it/p.mazzeo/sander_orca.git",
-    author="Patrizia Mazzeo, Edoardo Cignoni",
+    name="ml_server",
+    version="0.1.0",
+    url="https://github.com/Molecolab-Pisa/ML-server",
+    author="Patrizia Mazzeo and Edoardo Cignoni and Lorenzo Cupellini and Benedetta Mennucci",
     author_email="mazzeo.patrizia.1998@gmail.com, edoardo.cignoni96@gmail.com",
     description=open("README.md").read(),
     entry_points=entry_points,
